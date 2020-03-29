d7069119e7d0855ef80290391bc0d6c82586fd96
ahmadkadi1 <ahmad_kadi@live.com>
2020-03-29 15:22:18 +0300

    Updates

---
+++ precache-manifest.cc5c32ffff3474a6bf5f23c34825ae32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c6a24ee5c67354eb714e651fe96c56",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3fae6bd0c26b32a4c680",
+    "url": "/monsters-rolodex/static/css/main.a8eb1fc6.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fae6bd0c26b32a4c680",
+    "url": "/monsters-rolodex/static/js/main.33ce25e2.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
